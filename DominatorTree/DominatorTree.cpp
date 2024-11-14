a6528e906f5ee1f1df1dd58113ac8040f7eb577b
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2024-11-14 15:38:43 +0800

    bruteforce and quadratic algorithm

---
+++ DominatorTree/DominatorTree.cpp
@@ -0,0 +1,134 @@
+#include <assert.h>
+#include <iostream>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+struct e {
+  int u, v;
+  e() = default;
+  e(int a, int b) : u(a), v(b) {}
+  bool operator<(const e &e) const { return (u == e.u ? v < e.v : u < e.u); }
+};
+using vvi = vector<vector<int>>;
+constexpr int N = 1000;
+random_device rd;
+mt19937_64 gen(rd());
+bitset<N> nodes;
+vector<e> get_random_edges() {
+  uniform_int_distribution<> dist(10, 50);
+  int len = dist(gen);
+  set<e> s;
+  s.emplace(0, 1);
+  uniform_int_distribution<> d(1, N - 1);
+  for (int i = 1; i < len; i++) {
+    int a = d(gen), b = d(gen);
+    if (a == b)
+      b = (b + 1) % (N - 1) + 1;
+    assert(0 <= b && b < N);
+    s.emplace(a, b);
+  }
+
+  vector<e> edges(s.begin(), s.end());
+  return edges;
+}
+
+bitset<N> vis;
+void dfs(int u, int del, const vvi &G) {
+  assert(u >= 0 && u <= N - 1);
+  vis[u] = 1;
+  for (int v : G[u]) {
+    if (vis[v] || v == del)
+      continue;
+    dfs(v, del, G);
+  }
+}
+void connected(int u, const vvi &G) {
+  nodes[u] = 1;
+  for (int v : G[u]) {
+    if (nodes[v])
+      continue;
+    connected(v, G);
+  }
+}
+vector<bitset<N>> bruteforce(const vector<vector<int>> &G) {
+  vector<bitset<N>> dom(N);
+  for (int i = 1; i < N; i++) {
+    vis.reset();
+    if (!nodes[i])
+      continue;
+    dfs(0, i, G);
+    for (int j = 0; j < N; j++) {
+      if (!nodes[j])
+        continue;
+      dom[j].set(0);
+      if (!vis[j] && nodes[j])
+        dom[j].set(i);
+    }
+  }
+  return dom;
+}
+
+// quadratic time
+vector<bitset<N>> flow(const vector<vector<int>> &rG) {
+  vector<bitset<N>> dom(N);
+  bool changed = true;
+  dom[0][0] = 1;
+  for (int i = 1; i < N; i++) {
+    if (!nodes[i])
+      continue;
+    dom[i].set();
+  }
+  while (changed) {
+    changed = false;
+    for (int i = 1; i < N; i++) {
+      if (!nodes[i])
+        continue;
+      bitset<N> tmp;
+      tmp.set();
+      for (int p : rG[i]) {
+        if (nodes[p])
+          tmp &= dom[p];
+      }
+      tmp[i] = 1;
+      if (tmp != dom[i]) {
+        dom[i] = tmp;
+        changed = true;
+      }
+    }
+  }
+  return dom;
+}
+// Tarjan
+
+int main() {
+  vector<vector<int>> G(N);
+  vector<vector<int>> rG(N);
+  vector<e> edges = get_random_edges();
+  nodes.reset();
+  cout << rG.size() << endl;
+  for (auto edge : edges) {
+    G[edge.u].push_back(edge.v);
+    rG[edge.v].push_back(edge.u);
+  }
+  connected(0, G);
+  cout << nodes << endl;
+  // for (auto e : edges) {
+  //   if (!(nodes[e.u] && nodes[e.v]))
+  //     continue;
+  //   cout << e.u << ' ' << e.v << endl;
+  // }
+  auto ans = bruteforce(G);
+  auto res = flow(rG);
+  for (int i = 0; i < N; i++) {
+    if (!nodes[i])
+      continue;
+    if (ans[i] != res[i]) {
+      cout << i << " WA\n";
+      cout << ans[i] << endl;
+      cout << res[i] << endl;
+      break;
+    }
+  }
+  return 0;
+}
