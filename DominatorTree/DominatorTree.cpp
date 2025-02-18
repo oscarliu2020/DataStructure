505fbb1696dfeb6f3c96c01ad5a83346edfcea53
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2025-02-18 13:56:24 +0800

    add quadratic algorithm

--- DominatorTree/DominatorTree.cpp
+++ DominatorTree/DominatorTree.cpp
@@ -1,36 +1,74 @@
+#include <algorithm>
 #include <assert.h>
+#include <ctime>
 #include <iostream>
 #include <random>
 #include <set>
+#include <unordered_set>
 #include <vector>
 using namespace std;
-struct e {
-  int u, v;
-  e() = default;
-  e(int a, int b) : u(a), v(b) {}
-  bool operator<(const e &e) const { return (u == e.u ? v < e.v : u < e.u); }
-};
-using vvi = vector<vector<int>>;
-constexpr int N = 1000;
 random_device rd;
-mt19937_64 gen(rd());
-bitset<N> nodes;
-vector<e> get_random_edges() {
-  uniform_int_distribution<> dist(10, 50);
-  int len = dist(gen);
-  set<e> s;
-  s.emplace(0, 1);
-  uniform_int_distribution<> d(1, N - 1);
-  for (int i = 1; i < len; i++) {
-    int a = d(gen), b = d(gen);
-    if (a == b)
-      b = (b + 1) % (N - 1) + 1;
-    assert(0 <= b && b < N);
-    s.emplace(a, b);
+mt19937 gen(rd());
+using Adj = vector<vector<int>>;
+using vvi = vector<vector<int>>;
+constexpr int N = 20;
+Adj rand_comp(int n) {
+  vvi M(n, vector<int>(n, 0));
+  unordered_set<int> S;
+  //  for (int i = n - 1; i >= 0; i--) {
+  //    if (!S.empty()) {
+  //      for (int k = 0; k < 3; k++) {
+  //        uniform_int_distribution<> dist(0, S.size() - 1), rev(0, 1);
+  //        int v = dist(gen);
+  //        M[i][v] = 1;
+  //        int r = rev(gen);
+  //        if (r)
+  //          M[v][i] = 1;
+  //      }
+  //    }
+  //    S.insert(i);
+  //  }
+  S.insert(0);
+  for (int i = 1; i < n; i++) {
+    for (int k = 0; k < 3; k++) {
+      uniform_int_distribution<> dist(0, S.size() - 1), rev(0, 1);
+      int v = dist(gen);
+      M[v][i] = 1;
+      int r = rev(gen);
+      if (r)
+        M[i][v] = 1;
+    }
+    S.insert(i);
   }
+  Adj g(n);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (M[i][j])
+        g[i].push_back(j);
+    }
+  }
+  return g;
+}
 
-  vector<e> edges(s.begin(), s.end());
-  return edges;
+Adj rand_graph(int n, bitset<N> &exist) {
+  Adj g(n);
+
+  vector<pair<int, int>> pairs;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      pairs.emplace_back(i, j);
+    }
+  }
+  shuffle(pairs.begin(), pairs.end(), gen);
+  uniform_int_distribution<> dist(3, n);
+  int len = dist(gen);
+  for (int i = 0; i < len; i++) {
+    auto [u, v] = pairs[i];
+    g[u].push_back(v);
+    exist[u] = true;
+    exist[v] = true;
+  }
+  return g;
 }
 
 bitset<N> vis;
@@ -43,26 +81,15 @@ void dfs(int u, int del, const vvi &G) {
     dfs(v, del, G);
   }
 }
-void connected(int u, const vvi &G) {
-  nodes[u] = 1;
-  for (int v : G[u]) {
-    if (nodes[v])
-      continue;
-    connected(v, G);
-  }
-}
+
 vector<bitset<N>> bruteforce(const vector<vector<int>> &G) {
   vector<bitset<N>> dom(N);
   for (int i = 1; i < N; i++) {
     vis.reset();
-    if (!nodes[i])
-      continue;
     dfs(0, i, G);
     for (int j = 0; j < N; j++) {
-      if (!nodes[j])
-        continue;
       dom[j].set(0);
-      if (!vis[j] && nodes[j])
+      if (!vis[j])
         dom[j].set(i);
     }
   }
@@ -70,25 +97,20 @@ vector<bitset<N>> bruteforce(const vector<vector<int>> &G) {
 }
 
 // quadratic time
-vector<bitset<N>> flow(const vector<vector<int>> &rG) {
+vector<bitset<N>> flow1(const vector<vector<int>> &rG) {
   vector<bitset<N>> dom(N);
   bool changed = true;
   dom[0][0] = 1;
   for (int i = 1; i < N; i++) {
-    if (!nodes[i])
-      continue;
     dom[i].set();
   }
   while (changed) {
     changed = false;
     for (int i = 1; i < N; i++) {
-      if (!nodes[i])
-        continue;
       bitset<N> tmp;
       tmp.set();
       for (int p : rG[i]) {
-        if (nodes[p])
-          tmp &= dom[p];
+        tmp &= dom[p];
       }
       tmp[i] = 1;
       if (tmp != dom[i]) {
@@ -96,6 +118,88 @@ vector<bitset<N>> flow(const vector<vector<int>> &rG) {
         changed = true;
       }
     }
+  }
+  return dom;
+}
+void postorder(int u, const Adj &G, vector<int> &po) {
+  vis[u] = 1;
+  for (int v : G[u]) {
+    if (!vis[v])
+      postorder(v, G, po);
+  }
+  po.push_back(u);
+}
+vector<bitset<N>> flow2(const Adj &rG, const vector<int> &po) {
+  bool changed = true;
+  vector<bitset<N>> dom(N);
+  dom[0][0] = 1;
+  for (int i = 1; i < N; i++)
+    dom[i].set();
+  while (changed) {
+    changed = false;
+    for (int i = 1; i < po.size(); i++) {
+      int u = po[i];
+      bitset<N> tmp;
+      tmp.set();
+      for (int p : rG[u]) {
+        tmp &= dom[p];
+      }
+      tmp[u] = 1;
+      if (dom[u] != tmp) {
+        dom[u] = tmp;
+        changed = true;
+      }
+    }
+  }
+  return dom;
+}
+int intersect(int a, int b, const vector<int> &idom, const array<int, N> &pi) {
+  while (a != b) {
+    if (pi[a] < pi[b])
+      a = idom[a];
+    else
+      b = idom[b];
+  }
+  assert(a != -1);
+  return a;
+}
+// optimized
+vector<bitset<N>> flow3(const Adj &rG, const vector<int> &po,
+                        const array<int, N> &pi) {
+  vector<int> idom(N, -1);
+  bool changed = true;
+  idom[0] = 0;
+  while (changed) {
+    changed = false;
+    assert(po[0] == 0);
+    for (int i = 1; i < po.size(); i++) {
+      int u = po[i];
+      int j = 0;
+      for (j = 0; j < rG[u].size(); j++)
+        if (idom[rG[u][j]] != -1)
+          break;
+      int new_dom = idom[rG[u][j]];
+      for (; j < rG[u].size(); j++) {
+        int v = rG[u][j];
+        if (idom[v] == -1)
+          continue;
+        new_dom = intersect(new_dom, v, idom, pi);
+      }
+      if (idom[u] != new_dom) {
+        changed = true;
+        idom[u] = new_dom;
+      }
+    }
+  }
+  vector<bitset<N>> dom(N);
+  for (int i = 0; i < N; i++) {
+    int tmp = i;
+    do {
+      dom[i].set(tmp);
+      tmp = idom[tmp];
+      // assert(tmp != -1);
+    } while (tmp > 0);
+    dom[i][0] = 1;
   }
   return dom;
 }
@@ -108,35 +212,82 @@ vector<bitset<N>> flow(const vector<vector<int>> &rG) {
 /// dominates node v Let e(u) be the entry time in node u doing a Euler tour
 /// semi-dominator of u ,sdom(u), be a v such that there's a path from v to u
 /// and e(i)>=e(u) for every intermidate node i along the path from u to v
-
-int main() {
-  vector<vector<int>> G(N);
-  vector<vector<int>> rG(N);
-  vector<e> edges = get_random_edges();
-  nodes.reset();
-  cout << rG.size() << endl;
-  for (auto edge : edges) {
-    G[edge.u].push_back(edge.v);
-    rG[edge.v].push_back(edge.u);
-  }
-  connected(0, G);
-  cout << nodes << endl;
-  // for (auto e : edges) {
-  //   if (!(nodes[e.u] && nodes[e.v]))
-  //     continue;
-  //   cout << e.u << ' ' << e.v << endl;
-  // }
+void test() {
+  Adj G = rand_comp(N);
   auto ans = bruteforce(G);
-  auto res = flow(rG);
+  Adj Gr(N);
+  for (int u = 0; u < N; u++) {
+    for (int v : G[u])
+      Gr[v].push_back(u);
+  }
   for (int i = 0; i < N; i++) {
-    if (!nodes[i])
-      continue;
-    if (ans[i] != res[i]) {
-      cout << i << " WA\n";
-      cout << ans[i] << endl;
-      cout << res[i] << endl;
-      break;
+    for (int v : G[i]) {
+      cout << i << "->" << v << endl;
     }
   }
+  for (int i = 0; i < N; i++)
+    cout << ans[i].to_string() << ' ';
+  cout << endl;
+  {
+    clock_t start = clock();
+    auto f1 = flow1(Gr);
+    cout << "flow1() Time: " << (clock() - start) / (double)CLOCKS_PER_SEC
+         << endl;
+    assert(f1 == ans);
+  }
+  {
+
+    clock_t start = clock();
+    vis.reset();
+    vector<int> po;
+    postorder(0, G, po);
+    reverse(po.begin(), po.end());
+    auto f2 = flow2(Gr, po);
+    cout << "flow2() Time: " << (clock() - start) / (double)CLOCKS_PER_SEC
+         << endl;
+    if (ans != f2) {
+      cout << "ans\n";
+      for (int i = 0; i < N; i++) {
+        cout << ans[i].to_string() << ' ';
+      }
+      cout << endl << "f2\n";
+      for (int i = 0; i < N; i++) {
+        cout << f2[i].to_string() << ' ';
+      }
+      cout << endl;
+    }
+  }
+  {
+    clock_t start = clock();
+    vis.reset();
+    vector<int> po;
+    postorder(0, G, po);
+    array<int, N> pi{0};
+    for (int i = 0; i < po.size(); i++) {
+      pi[po[i]] = i;
+    }
+    reverse(po.begin(), po.end());
+    auto f3 = flow3(Gr, po, pi);
+    cout << "flow3() Time: " << (clock() - start) / (double)CLOCKS_PER_SEC
+         << endl;
+    if (f3 != ans) {
+      for (int i = 0; i < N; i++) {
+        for (int v : G[i]) {
+          cout << i << "->" << v << endl;
+        }
+      }
+      cout << "ans\n";
+      for (int i = 0; i < N; i++)
+        cout << ans[i].to_string() << ' ';
+      cout << endl << "f3\n";
+      for (int i = 0; i < N; i++)
+        cout << f3[i].to_string() << ' ';
+      cout << endl;
+    }
+  }
+}
+int main() {
+  for (int i = 0; i < 1; i++)
+    test();
   return 0;
 }
