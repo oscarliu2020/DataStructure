11600699e31d4c31d4e65d3a5aaeddca588686a8
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2025-03-04 16:34:25 +0800

    optimize SLT

--- DominatorTree/tarjan_test_2.cpp
+++ DominatorTree/tarjan_test_2.cpp
@@ -4,86 +4,102 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-using Adj = vector<vector<int>>;
 constexpr int N = 200001;
-int e[N], o[N];
-int cnt;
-int label[N], fa[N], pa[N];
-void dfs(int u, int p, const Adj &G, Adj &rG) {
-  e[u] = ++cnt;
-  o[cnt] = u;
-  label[u] = u;
-  pa[u] = p;
-  fa[u] = u;
-  for (int v : G[u]) {
-    if (!e[v])
-      dfs(v, u, G, rG);
-    rG[v].push_back(u);
+using Adj = vector<vector<int>>;
+struct Edge {
+  int v, next;
+};
+struct Graph {
+  Edge edges[N];
+  int head[N];
+  int size = 0;
+  Graph(int n) { memset(head, -1, n * sizeof(int)); }
+  void add_edge(int u, int v) {
+    int e = size++;
+    edges[e] = {v, head[u]};
+    head[u] = e;
   }
-}
-void compress(int v) {
-  if (fa[fa[v]] != fa[v]) {
-    compress(fa[v]);
-    if (e[label[fa[v]]] < e[label[v]]) {
-      label[v] = label[fa[v]];
+};
+class SLT {
+  vector<int> e, o, label, fa, pa;
+  int n, cnt;
+  Graph G, rG;
+  void dfs(int u) {
+    e[u] = ++cnt;
+    o[cnt] = u;
+    for (int x = G.head[u]; ~x; x = G.edges[x].next) {
+      int v = G.edges[x].v;
+      if (!e[v]) {
+        dfs(v);
+        pa[v] = u;
+      }
+      rG.add_edge(v, u);
     }
-    fa[v] = fa[fa[v]];
   }
-}
-int find(int v) {
-  if (v == fa[v])
-    return v;
-  compress(v);
+  int find(int u, bool m = 0) {
+    if (fa[u] == u)
+      return m ? -1 : u;
+    int v = find(fa[u], m || 1);
+    if (v < 0)
+      return m ? fa[u] : label[u];
+    if (e[label[fa[u]]] < e[label[u]])
+      label[u] = label[fa[u]];
+    fa[u] = v;
+    return m ? v : label[u];
+  }
+  int link(int v, int w) { return fa[w] = v; }
 
-  return label[v];
-}
-int dom[N];
-inline void link(int w, int v) { fa[v] = w; }
-void tarjan(int s, const Adj &G) {
-  Adj rG(G.size());
-  dfs(s, -1, G, rG);
-  assert(e[s] == 1);
-  assert(o[1] == s);
-  Adj t(G.size());
-  memset(dom, -1, G.size() * sizeof(int));
-  for (int i = cnt; i >= 2; i--) {
-    int w = o[i];
-    assert(pa[w] != -1);
-    for (int v : rG[w]) {
-      int u = find(v);
-      assert(e[u] && e[v]);
-      e[w] = min(e[u], e[w]);
+public:
+  SLT(int n)
+      : G(n), rG(n), n(n), cnt(0), e(n), o(n + 1), label(n), fa(n), pa(n) {}
+  void add_edge(int u, int v) { G.add_edge(u, v); }
+  vector<int> solve(int s) {
+    dfs(s);
+    Adj t(n);
+    vector<int> dom(n, -1);
+    for (int i = 0; i < n; i++) {
+      label[i] = fa[i] = i;
     }
-    link(pa[w], w);
-    t[o[e[w]]].push_back(w);
-    for (int v : t[pa[w]]) {
-      int u = find(v);
-      assert(e[v] && e[u]);
-      if (e[v] > e[u])
-        dom[v] = u;
-      else
-        dom[v] = pa[w];
+    for (int i = cnt; i >= 2; i--) {
+      int w = o[i];
+      for (int x = rG.head[w]; ~x; x = rG.edges[x].next) {
+        int v = rG.edges[x].v;
+        int u = find(v);
+        e[w] = min(e[w], e[u]);
+      }
+      link(pa[w], w);
+      t[o[e[w]]].push_back(w);
+      for (int v : t[pa[w]]) {
+        int u = find(v);
+        if (e[u] == e[v])
+          dom[v] = pa[w];
+        else {
+          assert(e[u] < e[v]);
+          dom[v] = u;
+        }
+      }
+      t[pa[w]].clear();
     }
-    t[pa[w]].clear();
+    for (int i = 2; i <= cnt; i++) {
+      int v = o[i];
+      if (dom[v] != o[e[v]])
+        dom[v] = dom[dom[v]];
+    }
+    dom[s] = s;
+    return dom;
   }
-  for (int i = 2; i <= cnt; i++) {
-    int w = o[i];
-    if (dom[w] != o[e[w]])
-      dom[w] = dom[dom[w]];
-  }
-  dom[s] = s;
-}
+};
 int main() {
   int n, m, s;
   cin.tie(0)->sync_with_stdio(0);
   cin >> n >> m >> s;
-  Adj G(n), rG(n);
+  SLT sol(n);
   for (int i = 0; i < m; i++) {
     int u, v;
     cin >> u >> v;
-    G[u].push_back(v);
+    sol.add_edge(u, v);
   }
-  tarjan(s, G);
+  auto dom = sol.solve(s);
   for (int i = 0; i < n; i++) {
     cout << dom[i] << ' ';
   }
